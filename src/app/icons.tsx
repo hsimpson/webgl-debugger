f2dcaee45ddef3b4de8510629f83e504963152c5
Daniel Toplak <homer.simpson123@gmx.de>
2020-01-14 19:51:41 +0100

    fix: import of fa icons only needed icons are included in the bundle

--- src/app/icons.tsx
+++ src/app/icons.tsx
@@ -1,7 +1,13 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCogs, faRocket } from '@fortawesome/free-solid-svg-icons';
-import { faQuestionCircle, faCopy, faImage } from '@fortawesome/free-regular-svg-icons';
-import { faBuffer } from '@fortawesome/free-brands-svg-icons';
+
+import { faCogs } from '@fortawesome/free-solid-svg-icons/faCogs';
+import { faRocket } from '@fortawesome/free-solid-svg-icons/faRocket';
+
+import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons/faQuestionCircle';
+import { faCopy } from '@fortawesome/free-regular-svg-icons/faCopy';
+import { faImage } from '@fortawesome/free-regular-svg-icons/faImage';
+
+import { faBuffer } from '@fortawesome/free-brands-svg-icons/faBuffer';
 
 // this is used to add all from FA
 library.add(faCogs, faRocket, faQuestionCircle, faCopy, faBuffer, faImage);
